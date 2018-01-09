f1d15d55320a2b51274f712ded85421479218305
Darren Kwong <kwongdarren@gmail.com>
2018-01-08 18:32:44 -0800

    updated ui

--- react-ui/src/components/Restdetails/Restheader.js
+++ react-ui/src/components/Restdetails/Restheader.js
@@ -9,7 +9,7 @@ export const Restheader = props => (
 				<p className='restaurant-address'>{props.address}, {props.city}, {props.state}</p>		      						
 			</div>
 			<div className='column auto'>
-				<p className='rank-section'>Trending Rank: <span className='rank-number'>7</span></p>
+				<span className='rank-section'>Trending Rank: <span className='rank-number'>7</span></span><br/>
 				<span><a target='blank' href={props.yelpURL}> Yelp Stars</a>: {props.yelpRating} &#124; </span>
 				<span> FB Rating: {props.fbRating}</span>				
 			</div>		
--- react-ui/src/components/chartFilter/chartFilter.js
+++ react-ui/src/components/chartFilter/chartFilter.js
@@ -2,13 +2,13 @@ import React from "react";
 import "./chartFilter.css";
 
 export const chartFilter = props => (
-  <div className="filter" {...props}>
+  <div className="chart-filter" {...props}>
   	{props.children}
   	<button className='button'{...props}> Similar Categories</button>
-  	<button className='button'{...props}> $ </button>
-  	<button className='button'{...props}> $$ </button>
-  	<button className='button'{...props}> $$$ </button>
-  	<button className='button'{...props}> $$$$ </button>	 
+  	<button className='button' value='$' onClick={props.checkClick}> $ </button>
+  	<button className='button' value='$' onClick={props.checkClick}> $$ </button>
+  	<button className='button' value='$' onClick={props.checkClick}> $$$ </button>
+  	<button className='button' value='$' onClick={props.checkClick}> $$$$ </button>	 
   </div>
 )
 
