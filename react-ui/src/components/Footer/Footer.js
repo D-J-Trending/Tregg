093795050ad03201b1f8f8739e681a591e10152a
Stephen Ruan <Stephen@Stephens-MacBook-Pro.local>
2018-01-13 03:44:31 -0800

    footer

--- react-ui/src/components/Footer/Footer.js
+++ react-ui/src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 
 const Footer = () =>
 <footer className="footer">
-		<p className="centered">Copyright &#169; 2017 | Trending Restaurants</p>
+		<p className="centered">Copyright &#169; 2017 | Tregg</p>
 </footer>
 
 export default Footer;
